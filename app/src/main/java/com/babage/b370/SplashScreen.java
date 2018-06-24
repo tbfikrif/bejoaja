d99045b396a6e2a324a5671ff6fae9214188ea3e
tbfikrif <tbfikrif@gmail.com>
2018-06-25 01:25:20 +0700

    Splash Screen

--- app/src/main/java/com/babage/b370/SplashScreen.java
+++ app/src/main/java/com/babage/b370/SplashScreen.java
@@ -1,13 +1,15 @@
 package com.babage.b370;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 
-public class SplashScreen extends AppCompatActivity {
+public class SplashScreen extends Activity {
     TextView tvSplash;
 
     @Override
@@ -16,6 +18,8 @@ public class SplashScreen extends AppCompatActivity {
 
         //menghilangkan ActionBar
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
 
         tvSplash = findViewById(R.id.tvSplash);
