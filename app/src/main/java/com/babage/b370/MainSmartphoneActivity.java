74e60270c1c134faca140de22cb14fe02537d6f1
tbfikrif <tbfikrif@gmail.com>
2018-07-09 23:01:34 +0700

    Layout Splash, Main Menu

--- app/src/main/java/com/babage/b370/MainSmartphoneActivity.java
+++ app/src/main/java/com/babage/b370/MainSmartphoneActivity.java
@@ -1,14 +1,12 @@
 package com.babage.b370;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
-public class SplashScreen extends Activity {
+public class MainSmartphoneActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,15 +16,6 @@ public class SplashScreen extends Activity {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_splash_screen);
-
-        final Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                finish();
-            }
-        }, 3000L); //3000 L = 3 detik
+        setContentView(R.layout.activity_main_smartphone);
     }
 }
